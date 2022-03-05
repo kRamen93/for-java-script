c328a5c494f5ec3adc29eaa9f92343dc69978f48
kRamen93 <96386297+kRamen93@users.noreply.github.com>
2022-03-05 19:32:24 +0300

    Add files via upload

---
+++ script_6.js
@@ -0,0 +1,57 @@
+// Объекты
+
+// let films = {
+//     2020: '1',
+//     2021: '2',
+//     2022: '3',
+//     2019: '4',
+//     2018: '5',
+// }
+
+// let car = {
+//     speed: '320 km/h',
+//     price: '10.000 $',
+//     color: 'Black color',
+//     model: 'BMW M5',
+//     equipment: 'Elegance',
+//     filmObject: films,
+// // }
+
+
+
+
+let computer = {
+    cpu: 'AMD Ryzen 5 3600 3200Ghz',
+    gpu: 'rx 580 6gb',
+    ram: '8gb',
+    ssd: '256gb',
+    typeSystem: 64,
+    // carObject: {...car},
+    
+}
+// переприсваиваем значение ключа hdd
+// computer.hdd = '512gb';
+
+// computer.ssd = '512gb';
+
+// console.log(computer);
+
+
+// console.log(Object.keys(computer));
+// console.log('=====================');
+// console.log(Object.values(computer));
+// console.log('=====================');
+// console.log(Object.entries(computer));
+// let a = 5;
+
+// let girls = {
+//     Katya: 7,
+//     Liza: a,
+//     Tanya: 6,
+//     Elana: 10,
+//     Valya: 5,
+// }
+
+// console.log(girls);
+
+// console.log(girls.Liza);
